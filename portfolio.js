6461ea079c408c7596e2d9d4cfcc43348c46daf4
Katelyn Wardell <kewardell14@gmail.com>
2025-04-02 14:18:20 -0600

    fixed a lot of javascript

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,31 @@
 import homegallery from "./portfolio.mjs";
 
 
+function displayhomegallery(homegallery) { //This sets up the gallery
+    return `<figure class="homegallery">
+	<a href="${homegallery.ahref}">
+        <img src="${homegallery.image}" alt="${homegallery.name}"/>
+    </a>
+</figure>`;
+}
+
+function rendergallery(homegallery) { //Displays the gallery
+    // get the element we will output the recipes into
+    let output = document.querySelector("#gallery");
+    // use the recipeTemplate function to transform our recipe objects into recipe HTML strings
+    let html = homegallery.map(i => displayhomegallery(i)).join("");
+    // Set the HTML strings as the innerHTML of our output element.
+    output.innerHTML = html;
+}
+
+
+const menuButton = document.getElementById("menu-button");
+const menuItems = document.getElementById("menu-items");
+menuButton.addEventListener("click", () => { // For Dropdown
+    menuItems.classList.toggle('hide');
+});
+
+
 
 const themeSelector = document.querySelector("select") // replace with code to select dropdown element out of the HTML (Hint: document.querySelector)
 function changeTheme() {
@@ -16,3 +41,6 @@ function changeTheme() {
     }
 }
 themeSelector.addEventListener('change', changeTheme);
+
+
+rendergallery(homegallery)
--- portfolio.mjs
+++ portfolio.mjs
@@ -1,10 +1,10 @@
 const homegallery = [
-    { name: "Outlet Cover", ahref: "outlet.html", image: "images/outlet/Main Cover Mockup.jpg" },
-    { name: "Banking Dashboard", ahref: "banking.html", image: "images/banking/Mobile Dashboard.jpg" },
-    { name: "General Conference Booklet", ahref: "gcbooklet.html", image: "images/optimized images/GC Booklet Cover.jpg" },
-    { name: "Hamlet Type Book Cover", ahref: "hamlet.html", image: "images/hamlet/Hamlet Type Cover Mockup velvet.jpg" },
-    { name: "Archery Logo", ahref: "archery.html", image: "images/optimized images/Minor Adjustments Archery Logo.png" },
-    { name: "Etsy Logo", ahref: "etsy.html", image: "images/optimized images/etsy-logo.jpg" }
+    { name: "Outlet", ahref: "outlet.html", image: "./images/outlet/Main Cover Mockup.jpg" },
+    { name: "Banking Website", ahref: "banking.html", image: "./images/banking/Mobile Dashboard.jpg" },
+    { name: "General Conference Booklet", ahref: "gcbooklet.html", image: "./images/optimized images/GC Booklet Cover.jpg" },
+    { name: "Hamlet Book Covers", ahref: "hamlet.html", image: "./images/hamlet/Hamlet Type Cover Mockup velvet.jpg" },
+    { name: "Minor Adjustments Archery Logo", ahref: "archery.html", image: "./images/optimized images/Minor Adjustments Archery Logo.png" },
+    { name: "WhatInYarnationStore Etsy Logo", ahref: "etsy.html", image: "./images/optimized images/etsy-logo.jpg" }
 ];
 
 export default homegallery
