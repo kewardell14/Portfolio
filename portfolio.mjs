b89b15da5299a030d62e6131d78e18c4a4524010
Katelyn Wardell <kewardell14@gmail.com>
2025-10-18 20:31:24 -0600

    added my identity package to home page

--- portfolio.mjs
+++ portfolio.mjs
@@ -1,9 +1,9 @@
 const homegallery = [
+    { name: "Identity Package", ahref: "identitypackage.html", image: "./images/idpackage/everything.jpg" },
     { name: "Outlet", ahref: "outlet.html", image: "./images/outlet/Main Cover Mockup.jpg" },
     { name: "Banking Website", ahref: "banking.html", image: "./images/banking/Mobile Dashboard.jpg" },
     { name: "General Conference Booklet", ahref: "gcbooklet.html", image: "./images/optimized images/GC Booklet Cover.jpg" },
     { name: "Hamlet Book Covers", ahref: "hamlet.html", image: "./images/hamlet/Hamlet Type Cover Mockup velvet.jpg" },
-    { name: "Minor Adjustments Archery Logo", ahref: "archery.html", image: "./images/Archery Logo.jpg" },
     { name: "WhatInYarnationStore Etsy Logo", ahref: "etsy.html", image: "./images/optimized images/etsy-logo.jpg" }
 ];
 
